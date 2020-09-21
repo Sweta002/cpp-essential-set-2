edb59bb75cd8c2b6a6719f0c8a50d018532c646e
99002657 <71540929+99002657@users.noreply.github.com>
2020-09-21 16:51:04 +0530

    Add files via upload

---
+++ SET2/MyTime/Mytime_test.cc
@@ -0,0 +1,12 @@
+
+#include "Mytime.h"
+#include <gtest/gtest.h>
+
+TEST(Mytime,DisplayTest) {
+    Mytime a1(10,20,30);
+    std::string ExpectedOut=" Hours: 10Minutes: 20Seconds: 30";
+    testing::internal::CaptureStdout();
+    a1.display();
+    std::string ActualOut = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(ExpectedOut.c_str(), ActualOut.c_str());
+}
